a5c115556fc7e116996b633faf7f398a46c432e1
ZiriTizi <148444098+ZiriTizi@users.noreply.github.com>
2023-12-07 13:25:23 +0100

    Update kmers.py

--- kmers.py
+++ kmers.py
@@ -1,4 +1,3 @@
-
 def kmer2str(val, k):
     """ Transform a kmer integer into a its string representation
     :param int val: An integer representation of a kmer
@@ -16,5 +15,26 @@ def kmer2str(val, k):
 
 
 def stream_kmers(text, k):
-    # --- To complete ---
-    pass
+    #print('Begin stream kmer')
+    encodage ={'A':0 ,'C':1,'T':2, 'G':3}
+    mask = (1 << (k*2)) - 1
+    x = 0
+    rx = 0
+    i = 0
+    for letter in text:
+        y = encodage[letter] 
+        x <<= 2
+        x += y
+        x &= mask
+        i +=1
+        
+        rx >>= 2
+        complement = (y + 2) & 0b11
+        rx += complement << (k-1)*2
+
+        if(i > k - 1):
+            yield(min(x,rx))
+
+
+
+

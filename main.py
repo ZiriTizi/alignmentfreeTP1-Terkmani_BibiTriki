17afa07a98d6d5b86e609260b56e510f8e9830b7
ZiriTizi <148444098+ZiriTizi@users.noreply.github.com>
2023-12-22 22:23:52 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,22 +2,23 @@ from loading import load_directory
 from kmers import stream_kmers, kmer2str
 import time
 import psutil
+import heapq
+import math
 import numpy as np
-
 
 start = time.time()
 disk_before = psutil.disk_usage('/').used
 distance = np.zeros((5,5))
 
-def jaccard(fileA, fileB, k):
+def jaccard(s1, s2, k):
     j = 0
     taille_I , taille_U = 0,0
     dico = {}
     print('Start Jaccard')
-    for kmer in stream_kmers(fileA, k):
+    for kmer in s1:
         dico[kmer] = 1 if kmer not in dico else dico[kmer]+1
         taille_U += 1
-    for kmer in stream_kmers(fileB, k):
+    for kmer in s2:
         if kmer in dico:
             taille_I += 1
             if dico[kmer] != 0:
@@ -34,20 +35,41 @@ def jaccard(fileA, fileB, k):
 
 
 
+
+def echantillon(file, k, s):
+    h = [float('-inf')] * s  
+    heapq.heapify(h)
+
+    for kmer in stream_kmers(file, k):
+        if -kmer > min(h):
+            heapq.heappushpop(h, kmer) # the pop function pops the smallest value in the heap
+    return h
+
+
+
+
+
+
 if __name__ == "__main__":
     # Load all the files in a dictionary
-    files = load_directory("data")
-    k = 21
+    files = load_directory("C:/Users/Lenovo/Downloads/TP2/alignmentfreeTP1-main/data")
+    k = 32
+
+
 
     filenames = list(files.keys())
     print(filenames)
     for i in range(len(files)):
         for j in range(i+1, len(files)):
-            jacc = jaccard(files[filenames[i]][0], files[filenames[j]][0], k)
+            s1 = echantillon(files[filenames[i]][0],k,32)
+            #print(s)
+            s2 = echantillon(files[filenames[j]][0],k,32)
+            jacc = jaccard(s1, s2, k)
             print(filenames[i], filenames[j], jacc)
             distance[i,j]=jacc
-            
 print(distance)
+
+
 print('this actually took : ',time.time() - start)
 disk_after= psutil.disk_usage('/').used
 disk_used_by_program = disk_after-disk_before
